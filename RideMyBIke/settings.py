d7cb5a4debce464d337a3a6f388a84a181a33ff2
shahiljoshi <shahil.inexture@gmail.com>
2021-06-22 12:37:15 +0530

    fix:fb auth

--- RideMyBIke/settings.py
+++ RideMyBIke/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = 'django-insecure-z2=_p#r%rrd2cijgoxt^)9_*60t!@c4o=%*15-el-&%)wyp^k4'
-# SECRET_KEY = os.environ.get('SECRET_KEY_BIKE')
+SECRET_KEY = os.environ.get('SECRET_KEY_BIKE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -206,7 +205,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-#EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-#EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_HOST_USER = 'shahiljoshi98@gmail.com'
-EMAIL_HOST_PASSWORD = 'shahilpersonal98'
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+
