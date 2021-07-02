0db70a771d2211b58396479f973e2c681f342e65
shahiljoshi <shahil.inexture@gmail.com>
2021-07-02 18:33:39 +0530

    fix:db settings changes

--- RideMyBIke/settings.py
+++ RideMyBIke/settings.py
@@ -123,25 +123,25 @@ WSGI_APPLICATION = 'RideMyBIke.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-#
 # DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql',
-#             'NAME': 'd5aj44gsj9759k',
-#             'USER': 'lgnwuramuxnnhn',
-#             'PASSWORD': 'f2d8bc7774ddf4b5eda323d146b59e91b530d308c4d290d3297fbe3a13ab47a1',
-#             'HOST': 'ec2-34-232-191-133.compute-1.amazonaws.com',
-#             'PORT': 5432,
-#
-#         }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
+# }
+
+
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'd5aj44gsj9759k',
+            'USER': 'lgnwuramuxnnhn',
+            'PASSWORD': 'f2d8bc7774ddf4b5eda323d146b59e91b530d308c4d290d3297fbe3a13ab47a1',
+            'HOST': 'ec2-34-232-191-133.compute-1.amazonaws.com',
+            'PORT': 5432,
+
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
